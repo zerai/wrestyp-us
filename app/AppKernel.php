8534ade7fdd32676e4ec8b919dbcbcf82c2e0ebc
zerai <teclaizerai@gmail.com>
2013-03-07 13:29:44 +0000

    Add ZoonetBundle + PageController

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Zoonet\YpusBundle\ZoonetYpusBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
